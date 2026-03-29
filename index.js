be7a1688c3f3380f9fff0ff5d4ee5d119ab977a2
Sasha <aleksandrpodlesnyi09@gmail.com>
2026-03-29 23:04:07 +0300

    page Vizytka the end but last btns EN and UA

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// копіювання буде на номер телефону та пошта + login та password 
 
+
+// копіювання номера телефону та пошти
 function copyText(id) {
   const textToCopy = document.getElementById(id);
   if (textToCopy) {
@@ -10,3 +11,29 @@ function copyText(id) {
     });
   }
 }
+// Слайдер відгуків
+function slider() {
+  let offset = 0;
+  const sliderLine = document.querySelector('.slider-line');
+
+  document.querySelector('#arrowRight').addEventListener('click', function () {
+    offset += 830;
+    if (offset > 2500) {
+      offset = 0;
+
+    }
+    sliderLine.style.left = -offset + 'px';
+  });
+  document.querySelector('#arrowLeft').addEventListener('click', function () {
+    offset -= 830;
+    if (offset < 0) {
+      offset = 2490;
+
+    }
+    sliderLine.style.left = -offset + 'px';
+  });
+}
+
+
+
+slider();
