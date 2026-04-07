b788f01920f01c177528782a0fc3e29d4a3d9fdf
Sasha <aleksandrpodlesnyi09@gmail.com>
2026-04-07 11:41:46 +0300

    dev

---
+++ Home.js
@@ -0,0 +1,34 @@
+// копіювання буде на номер телефону та пошта + login та password 
+
+function copyText(id) {
+  const textToCopy = document.getElementById(id);
+  if (textToCopy) {
+    navigator.clipboard.writeText(textToCopy.innerText).then(() => {
+      alert('Текст скопійовано!');
+    }).catch(err => {
+      console.error('Помилка копіювання: ', err);
+    });
+  }
+}
+
+$('#rulSkill').hover(function () {
+  $('#rulSkill').css('background', " #5cc7dc");
+  $('.blockSkill').css('background', "#8ECAFF");
+  $('#rulerSkill').css('display', "flex");
+  $('.blockSkill').mouseleave(function () {
+    $('#rulSkill').css('background', "#84E0F3");
+    $('.blockSkill').css('background', "none");
+    $('#rulerSkill').css('display', "none");
+  });
+});
+
+$('#rulAbout').hover(function () {
+  $('#rulAbout').css('background', " #5cc7dc");
+  $('.blockAbout').css('background', "#8ECAFF");
+  $('#rulerAbout').css('display', "flex");
+  $('.blockAbout').mouseleave(function () {
+    $('#rulAbout').css('background', "#84E0F3");
+    $('.blockAbout').css('background', "none");
+    $('#rulerAbout').css('display', "none");
+  });
+});
