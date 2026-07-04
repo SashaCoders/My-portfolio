e40d026c7d3a4e686bd4c0d0f7b5916325dfef9f
Sasha <aleksandrpodlesnyi09@gmail.com>
2026-07-04 22:51:23 +0300

    I dinished page Skill

--- Skill.js
+++ Skill.js
@@ -11,28 +11,15 @@ function copyText(id) {
   }
 }
 
-$('#rulSkill').hover(function () {
-  $('#rulSkill').css('background', " #5cc7dc");
-  $('.blockSkill').css('background', "#8ECAFF");
-  $('#rulerSkill').css('display', "flex");
-  $('.blockSkill').mouseleave(function () {
-    $('#rulSkill').css('background', "#84E0F3");
-    $('.blockSkill').css('background', "none");
-    $('#rulerSkill').css('display', "none");
-  });
-
-
-
-
-});
-
 $('#rulAbout').hover(function () {
-  $('#rulAbout').css('background', " #5cc7dc");
-  $('.blockAbout').css('background', "#8ECAFF");
+  $('#rulAbout').css('background', " #F3F3FB");
+  $('.blockAbout').css('background', "#F9F9FC");
+   $('.blockAbout').css('box-shadow', "0 2px 4px 3px rgb(190 177 241 / 0.4)");
   $('#rulerAbout').css('display', "flex");
   $('.blockAbout').mouseleave(function () {
-    $('#rulAbout').css('background', "#84E0F3");
+    $('#rulAbout').css('background', "#EBEBFD");
     $('.blockAbout').css('background', "none");
+       $('.blockAbout').css('box-shadow', "none");
     $('#rulerAbout').css('display', "none");
   });
 });
